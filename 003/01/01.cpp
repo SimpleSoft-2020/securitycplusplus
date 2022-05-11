93478ecfca619cc48692a8ca95eb60fdb5cf1656
haiyang <zhanghaiyang9999@gmail.com>
2022-05-11 18:15:15 +0800

    003-01

---
+++ 003/01/01.cpp
@@ -0,0 +1,37 @@
+﻿// 01.cpp: 定义应用程序的入口点。
+//
+
+#include "01.h"
+
+using namespace std;
+class test
+{
+public:
+	test()
+	{
+		cout << "test()" << endl;
+	}
+	test(int i)
+	{
+		cout << "test(i)" << i << endl;
+	}
+	virtual ~test()
+	{
+		cout << "~test()" << endl;
+	}
+};
+struct  test2
+{
+	test2(test t)
+	{
+		cout << "test2()" << endl;
+	}
+};
+
+int main()
+{
+	test c();
+	int i = 6;
+	test2 t(test(i) );	  //test2 t(test i)
+	return 0;
+}
