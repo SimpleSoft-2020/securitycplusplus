16e22fdd3ffd77c74603435a10fd12ffe0e614b9
haiyang <zhanghaiyang9999@gmail.com>
2022-05-13 13:45:02 +0800

    005

---
+++ 005/01/01.cpp
@@ -0,0 +1,20 @@
+﻿// 01.cpp: 定义应用程序的入口点。
+//
+
+#include "01.h"
+
+using namespace std;
+
+int test(int i)
+{
+	cout << "test fun" << i << endl;
+	return i*i;
+}
+int main()
+{
+	int a = 10;
+	int b = sizeof(a++);
+	int c = sizeof(test(10));
+	cout << std::is_same<decltype(test(10)), float>::value;			
+	return 0;
+}
